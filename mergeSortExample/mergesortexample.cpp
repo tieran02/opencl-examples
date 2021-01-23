48a319c2a6162bc0e9b3b80e4617631922fa2709
tieran02 <tieran.wightman@gmail.com>
2021-01-23 13:48:25 +0000

    merge sort for large sets

--- mergeSortExample/mergesortexample.cpp
+++ mergeSortExample/mergesortexample.cpp
@@ -11,33 +11,37 @@
 #include <stopwatch.h>
 #include <iostream>
 #include <util.h>
+#include <algorithm>
 
-template <size_t size> std::array<int,size> randomArray();
+template <size_t size> std::vector<int> randomNumbers();
 void NormalMergeSort();
 void cpuMergeSort(int* array, int leftIndex, int rightIndex);
 void cpuMerge(int* array, int leftIndex,int midIndex, int rightIndex);
+void stdSort();
 
 void ComputeMergeSort();
 
+constexpr int array_size = 1024000;
+
 int main()
 {
-    NormalMergeSort();
+    stdSort();
     ComputeMergeSort();
+    NormalMergeSort();
     return 0;
 }
 
 #pragma region cpuMergeSort
 void NormalMergeSort()
 {
-    constexpr int array_size = 25600;
-    std::array<int, array_size> array = randomArray<array_size>();
+    std::vector<int> array = randomNumbers<array_size>();
 
     Stopwatch stopwatch;
     stopwatch.Start();
     cpuMergeSort(array.data(),0,array.size()-1);
 
     stopwatch.Stop();
-    std::cout << "cpu merge sort " << stopwatch.Time() << std::endl;
+    std::cout << array_size << " int int cpu merge sort " << stopwatch.Time() << std::endl;
 }
 
 void cpuMergeSort(int *array, int leftIndex, int rightIndex)
@@ -106,10 +110,9 @@ void cpuMerge(int *array, int leftIndex, int midIndex, int rightIndex)
 
 void ComputeMergeSort()
 {
-    constexpr int local_work_size = 64;
-    constexpr int array_size = 256;
-    std::array<int, array_size> array = randomArray<array_size>();
-    std::array<int, array_size> sortedArray{};
+    constexpr int local_work_size = 16;
+    std::vector<int> array = randomNumbers<array_size>();
+    std::vector<int> sortedArray(array_size);
 
     cl_int err = CL_SUCCESS;
     try
@@ -117,16 +120,16 @@ void ComputeMergeSort()
         std::vector<cl::Platform> platforms;
         cl::Platform::get(&platforms);
         if (platforms.size() == 0) {
-            std::cout << "Platform size 0\n";
+            //std::cout << "Platform size 0\n";
             return;
         }
 
         // Print number of platforms and list of platforms
-        std::cout << "Platform number is: " << platforms.size() << std::endl;
+        //std::cout << "Platform number is: " << platforms.size() << std::endl;
         std::string platformVendor;
         for (unsigned int i = 0; i < platforms.size(); ++i) {
             platforms[i].getInfo((cl_platform_info)CL_PLATFORM_VENDOR, &platformVendor);
-            std::cout << "Platform is by: " << platformVendor << std::endl;
+            //std::cout << "Platform is by: " << platformVendor << std::endl;
         }
 
         cl_context_properties properties[] =
@@ -141,27 +144,26 @@ void ComputeMergeSort()
         std::vector<cl::Device> devices = context.getInfo<CL_CONTEXT_DEVICES>();
 
         // Print number of devices and list of devices
-        std::cout << "Device number is: " << devices.size() << std::endl;
+        //std::cout << "Device number is: " << devices.size() << std::endl;
         for (unsigned int i = 0; i < devices.size(); ++i) {
-            std::cout << "Device #" << i << ": " << devices[i].getInfo<CL_DEVICE_NAME>() << std::endl;
+            //std::cout << "Device #" << i << ": " << devices[i].getInfo<CL_DEVICE_NAME>() << std::endl;
         }
 
         //create buffers
-        cl::Buffer unsortedBuffer(context, CL_MEM_READ_ONLY, array.size() * sizeof(int), nullptr, &err);
-        cl::Buffer partialSortedBuffer(context, CL_MEM_READ_WRITE, array.size() * sizeof(int), nullptr, &err);
-        cl::Buffer sortedBuffer(context, CL_MEM_WRITE_ONLY, array.size() * sizeof(int), nullptr, &err);
+        cl::Buffer bufferA(context, CL_MEM_READ_WRITE, array.size() * sizeof(int), nullptr, &err);
+        cl::Buffer bufferB(context, CL_MEM_READ_WRITE, array.size() * sizeof(int), nullptr, &err);
 
         //copy unsorted data to buffer
         cl::CommandQueue queue(context, devices[0], 0, &err);
-        queue.enqueueWriteBuffer(unsortedBuffer, CL_TRUE, 0, array.size() * sizeof(int), array.data());
+        queue.enqueueWriteBuffer(bufferA, CL_TRUE, 0, array.size() * sizeof(int), array.data());
 
         std::string kernalStr = LoadFromFile("kernels/mergeSort.cl");
         const cl::Program::Sources source(1, std::make_pair(kernalStr.data(), kernalStr.size()));
         const cl::Program program = BuildProgram(context, devices, source);
 
         cl::Kernel sortKernel(program, "MergeSort", &err);
-        sortKernel.setArg(0, sizeof(unsortedBuffer), &unsortedBuffer);
-        sortKernel.setArg(1, sizeof(partialSortedBuffer), &partialSortedBuffer);
+        sortKernel.setArg(0, sizeof(bufferA), &bufferA);
+        sortKernel.setArg(1, sizeof(bufferB), &bufferB);
         sortKernel.setArg(2, sizeof(int), &array_size);
 
         cl::Kernel mergeKernel(program, "Merge", &err);
@@ -184,9 +186,11 @@ void ComputeMergeSort()
                 globalWorkSize,
                 localWorkSize);
 
+        //swap buffers, so partial sorted becomes the primary buffer
+        std::swap(bufferA,bufferB);
+
 
         //now continue with global merge
-        unsigned int locLimit = 2 * local_work_size;
         unsigned int stride = local_work_size;
 
         for(; stride <= array_size; stride <<= 1)
@@ -197,8 +201,8 @@ void ComputeMergeSort()
             localWorkSize = cl::NDRange(std::min(local_work_size, neededWorkers));
             globalWorkSize = GetGlobalSize(neededWorkers, localWorkSize[0]);
 
-            mergeKernel.setArg(0, sizeof(cl::Buffer), &partialSortedBuffer);
-            mergeKernel.setArg(1, sizeof(cl::Buffer), &sortedBuffer);
+            mergeKernel.setArg(0, sizeof(cl::Buffer), &bufferA);
+            mergeKernel.setArg(1, sizeof(cl::Buffer), &bufferB);
             mergeKernel.setArg(2, sizeof(cl_uint), &stride);
 
             queue.enqueueNDRangeKernel(
@@ -206,16 +210,19 @@ void ComputeMergeSort()
                     cl::NDRange(0),
                     globalWorkSize,
                     localWorkSize);
+
+            //swap buffers, so partial sorted becomes the primary buffer
+            std::swap(bufferA,bufferB);
         }
 
         cl::Event event;
-        queue.enqueueReadBuffer(sortedBuffer, true, 0, array_size * sizeof(int), sortedArray.data(), nullptr, &event);
+        queue.enqueueReadBuffer(bufferA, true, 0, array_size * sizeof(int), sortedArray.data(), nullptr, &event);
         //wait for event to finish
         event.wait();
         queue.finish();
 
         stopwatch.Stop();
-        std::cout << "compute merge sort " << stopwatch.Time() << std::endl;
+        std::cout << array_size << " int compute merge sort " << stopwatch.Time() << std::endl;
     }
     catch (cl::Error err)
     {
@@ -232,15 +239,28 @@ void ComputeMergeSort()
 
 #pragma endregion
 
-template<size_t size>
-std::array<int, size> randomArray() 
+void stdSort()
 {
-    std::array<int,size> array;
+    std::vector<int> array = randomNumbers<array_size>();
+
+    Stopwatch stopwatch;
+    stopwatch.Start();
+    std::sort(array.begin(), array.end());
+
+    stopwatch.Stop();
+    std::cout << array_size << " int int std sort " << stopwatch.Time() << std::endl;
+}
+
+template<size_t size>
+std::vector<int> randomNumbers()
+{
+    std::vector<int> array(size);
 
     for (int i = 0; i < size; ++i)
     {
-        array[i] = rand() % 100000 + 1;
+        array[i] = rand() % std::numeric_limits<int>::max();
     }
 
     return array;
 }
+
